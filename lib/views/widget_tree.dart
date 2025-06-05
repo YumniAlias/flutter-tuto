4636b1527169ecfbc4bb4682b9fbb519f9fdfcc1
YumniAlias <yumni@comm-itgroup.com>
2025-06-05 17:13:38 +0800

    first commit

---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tut/data/notifiers.dart';
+import 'package:flutter_tut/views/pages/home_page.dart';
+import 'package:flutter_tut/views/pages/profile_page.dart';
+import 'package:flutter_tut/views/pages/settings_page.dart';
+
+import 'widgets/navbar_widget.dart';
+
+List<Widget> pages = [HomePage(), ProfilePage()];
+
+class WidgetTree extends StatelessWidget {
+  const WidgetTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Mapp'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              isDarkModeNotifier.value = !isDarkModeNotifier.value;
+            },
+            icon: ValueListenableBuilder(
+              valueListenable: isDarkModeNotifier,
+              builder: (context, isDarkMode, child) {
+                return Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode);
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return SettingsPage(title: 'Settings qwer',);
+                  },
+                ),
+              );
+            },
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
+      bottomNavigationBar: NavbarWidget(),
+    );
+  }
+}
