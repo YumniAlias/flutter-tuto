3787b8ac2971c3e16336c637d75037503f79d9e8
YumniAlias <yumni@comm-itgroup.com>
2025-06-09 16:39:24 +0800

    Course finished

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,18 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tut/views/widgets/hero_widget.dart';
+import 'package:flutter_tut/data/classes/activity_class.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => CoursePageState();
+}
+
+class CoursePageState extends State<CoursePage> {
+  late Future<Activity> futureActivity;
+  bool isFirst = true;
+
+  @override
+  void initState() {
+    super.initState();
+    futureActivity = fetchActivity();
+  }
+
+  Future<Activity> fetchActivity() async {
+    final response = await http.get(
+      Uri.parse('https://bored-api.appbrewery.com/random'),
+    );
+
+    if (response.statusCode == 200) {
+      return Activity.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load activity');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: SingleChildScrollView(
-          child: Column(children: [HeroWidget(title: 'Course')]),
-        ),
+      appBar: AppBar(
+        title: const Text('Activity Viewer'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                isFirst = !isFirst;
+              });
+            },
+            icon: Icon(Icons.switch_access_shortcut),
+          ),
+        ],
+      ),
+      body: FutureBuilder<Activity>(
+        future: futureActivity,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final activity = snapshot.data!;
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: AnimatedCrossFade(
+                firstChild: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Activity: ${activity.activity}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text('Type: ${activity.type}'),
+                    Text('Participants: ${activity.participants}'),
+                    Text('Price: ${activity.price}'),
+                    Text('Availability: ${activity.availability}'),
+                    Text('Accessibility: ${activity.accessibility}'),
+                    Text('Duration: ${activity.duration}'),
+                    Text(
+                      'Kid Friendly: ${activity.kidFriendly ? "Yes" : "No"}',
+                    ),
+                    Text('Link: ${activity.link}'),
+                    Text('Key: ${activity.key}'),
+                  ],
+                ),
+                secondChild: Center(
+                  child: Image.asset('assets/images/appLogo.png'),
+                ),
+                crossFadeState: isFirst
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
+                duration: Duration(milliseconds: 1000),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            futureActivity = fetchActivity();
+          });
+        },
+        child: const Icon(Icons.refresh),
       ),
     );
   }
